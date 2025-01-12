240f934e61a456e0356d6b5ef19b460545d33899
Jinyang Wang <jinyang.wang27@outlook.com>
2025-01-12 10:53:25 +0100

    WIP on adding IChing interpretation

---
+++ src/ichingpy/divination/iching.py
@@ -0,0 +1,27 @@
+# %%
+import importlib.resources
+import json
+
+from ichingpy.divination.base import DivinationEngineBase
+from ichingpy.model.hexagram import Hexagram
+from ichingpy.model.interpretation.hexagram.iching_hexagram import IChingHexagramInterp
+
+
+class IChingDivinationEngine(DivinationEngineBase):
+
+    def __init__(self):
+        self._load_interpretation_data()
+
+    def _load_interpretation_data(self) -> None:
+        with importlib.resources.files("ichingpy.data").joinpath("iching_zh.json").open() as f:
+            self._data = json.load(f)["hexagrams"]
+
+    def execute(self, hexagram: Hexagram) -> None:
+        key = str(tuple([v % 2 for v in hexagram.values]))
+        interp = IChingHexagramInterp.model_validate(self._data[key])
+        for line, line_interp in zip(hexagram.lines, interp.lines):
+            line_interp.status = line.status
+        hexagram.interpretation = interp
+
+
+# %%
--- src/ichingpy/model/interpretation/hexagram/six_line_hexagram.py
+++ src/ichingpy/model/interpretation/hexagram/six_line_hexagram.py
@@ -1,5 +1,23 @@
 from ichingpy.model.interpretation.hexagram.base import HexagramInterpretationBase
+from ichingpy.model.interpretation.line.base import LineInterpretationBase
+from ichingpy.model.interpretation.line.six_line_line import SixLineLineInterp
 from ichingpy.model.interpretation.trigram.six_line_trigram import SixLineTrigramInterp
 
 
-class SixLineHexagramInterp(HexagramInterpretationBase[SixLineTrigramInterp]): ...
+class SixLineHexagramInterp(HexagramInterpretationBase[SixLineTrigramInterp]):
+    inner: SixLineTrigramInterp
+    outer: SixLineTrigramInterp
+
+    @property
+    def lines(self) -> list[SixLineLineInterp]:
+        """Get the lines of the Hexagram.
+        返回卦之六爻。
+        """
+        return self.inner.lines + self.outer.lines
+
+    def get_lines(self) -> list[LineInterpretationBase]:
+        # tODO: fix generic type
+        return self.lines  # type: ignore
+
+    def __repr__(self) -> str:
+        return "\n".join(repr(line) for line in self.lines[::-1])
