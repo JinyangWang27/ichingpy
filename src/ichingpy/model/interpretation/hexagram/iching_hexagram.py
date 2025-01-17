7d211aa5503a12f0438d9224cdb952f276a5c15d
Jinyang Wang <jinyang.wang27@outlook.com>
2025-01-17 14:51:58 +0100

    add a todo

--- src/ichingpy/model/interpretation/hexagram/iching_hexagram.py
+++ src/ichingpy/model/interpretation/hexagram/iching_hexagram.py
@@ -22,6 +22,14 @@ class IChingHexagramInterp(HexagramInterpretationBase[IChingTrigramInterp, IChin
     def get_lines(self) -> list[IChingLineInterp]:
         return self.lines
 
+    # TODO: implement the following
+    # 第一，凡卦的六爻皆不可变，就用筮得之卦的卦辞判断吉凶；
+    # 第二，若筮得之卦（本卦）中有一爻可变，就用本卦的这一爻的爻辞判断吉凶；
+    # 第三，本卦有二爻可变，用上一爻的爻辞为主来判断吉凶；
+    # 第四，有三爻可变，则用本卦及之卦的卦辞相结合判断吉凶；
+    # 第五，四爻可变，则用之卦中不变的二爻爻辞，并以下爻的爻辞为主来判断吉凶；
+    # 第六，五爻可变，用之卦中不变的一爻爻辞判断吉凶；
+    # 第七，六爻皆可变，乾坤二卦用“用爻”的爻辞判断吉凶，其他卦则用之卦的卦辞来判断吉凶。
     def __repr__(self) -> str:
         graph_repr = "\n".join(repr(line) for line in self.lines[::-1])
         text = ""
--- tests/unit/divination/test_iching_divination.py
+++ tests/unit/divination/test_iching_divination.py
@@ -1,10 +1,15 @@
+from itertools import product
+
+import pytest
+
 from ichingpy.divination.iching import IChingDivinationEngine
 from ichingpy.model.hexagram import Hexagram
 
 
-def test_iching_divination():
+@pytest.mark.parametrize("hexagram_int", product([0, 1], repeat=6))
+def test_iching_divination(hexagram_int: list[int]):
     engine = IChingDivinationEngine()
-    hexagram = Hexagram.from_binary([1, 1, 3, 1, 1, 3])
+    hexagram = Hexagram.from_binary(hexagram_int)
     engine = IChingDivinationEngine()
     engine.execute(hexagram)
     assert hexagram.interpretation is not None
